9b4e6527bae52d885ba037e6ccd9a9d6d3972c98
Ed Marciniak <ekmarciniak@gmail.com>
2022-12-14 15:54:42 -0600

    Add text color

--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ const root = document.querySelector("#root");
 
 const todo = await fetch("https://jsonplaceholder.typicode.com/todos");
 
-const list = await todo.json();
+const foxTrot = await todo.json();
 
-console.log(list);
+function createId(user) {
+    return `
+    <section class="bg-gradient p-8">
+    <h2 UserID: ${user.userId}</h2>
 
+    <ul>
+      <li>ID Number: ${user.id}</li>
+      <li>Title of User: ${user.title}</li>
+      <li>Task Complete: ${user.completed}</li>
+    </ul>
+   </section>
+    `;
+  }
+
+  const bioCardsHTML = foxTrot.map(createId).join("");
+
+  root.innerHTML = bioCardsHTML;
